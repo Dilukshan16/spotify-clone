2d5dd43b925230af4ffff1d27f66eafa553a06dd
Dilukshan <pdiluksan@gmail.com>
2025-07-11 09:59:48 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Sidebar from "./components/Sidebar";
+import Player from "./components/Player";
+
+const App = () => {
+  return (
+    <div className="h-screen bg-black">
+      <div className="h-[90%] flex">
+        <Sidebar />
+        <Player />
+      </div>
+    </div>
+  );
+};
+
+export default App;
