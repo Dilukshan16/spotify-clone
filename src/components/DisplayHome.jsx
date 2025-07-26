f0d236f22f50817125cb011a17fd03e296ff4b57
Dilukshan <pdiluksan@gmail.com>
2025-07-27 00:02:03 +0530

    display component created

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -1,38 +1,41 @@
 import React from "react";
 import Navbar from "./Navbar";
-import { albumsData } from "../assets/assets";
+import { albumsData, songsData } from "../assets/assets";
 import AlbumItem from "./AlbumItem";
-import { songsData } from "../assets/assets";
 import SongItem from "./SongItem";
 
 const DisplayHome = () => {
   return (
     <>
       <Navbar />
-      <div className="mb-4">
-        <h1 className="my-5 font-bold text-2xl ">Featured Charts</h1>
-        <div className="flex overflow-auto">
+      <div className="mb-4 px-4">
+        {/* Featured Charts Section */}
+        <h1 className="my-5 font-bold text-2xl">Featured Charts</h1>
+        <div className="flex overflow-x-auto space-x-4 pb-4">
           {albumsData.map((item, index) => (
-            <AlbumItem
-              key={index}
-              name={item.name}
-              desc={item.desc}
-              id={item.id}
-              image={item.image}
-            />
+            <div key={index} className="min-w-[200px]">
+              <AlbumItem
+                name={item.name}
+                desc={item.desc}
+                id={item.id}
+                image={item.image}
+              />
+            </div>
           ))}
         </div>
 
-        <h1 className="my-5 font-bold text-2xl ">Today's biggest hits </h1>
-        <div className="flex overflow-auto">
+        {/* Today's Hits Section */}
+        <h1 className="my-5 font-bold text-2xl">Today's biggest hits</h1>
+        <div className="flex overflow-x-auto space-x-4 pb-4">
           {songsData.map((item, index) => (
-            <SongItem
-              key={index}
-              name={item.name}
-              desc={item.desc}
-              id={item.id}
-              image={item.image}
-            />
+            <div key={index} className="min-w-[200px]">
+              <SongItem
+                name={item.name}
+                desc={item.desc}
+                id={item.id}
+                image={item.image}
+              />
+            </div>
           ))}
         </div>
       </div>
