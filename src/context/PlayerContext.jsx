0f1a47eaa7696a03e388c92fd79a7193d1ececf7
Dilukshan <pdiluksan@gmail.com>
2025-08-06 16:52:39 +0530

    choose songs in album using id

--- src/context/PlayerContext.jsx
+++ src/context/PlayerContext.jsx
@@ -1,11 +1,72 @@
-import React, { createContext, useEffect, useRef } from "react";
+import React, { createContext, use, useEffect, useRef, useState } from "react";
+import { songsData } from "../assets/assets";
 
 export const PlayerContext = createContext();
 const PlayerContextProvider = (props) => {
   const audioRef = useRef();
+  const seekBg = useRef();
+  const seekBar = useRef();
+  const [track, setTrack] = useState(songsData[0]);
+  const [playerStatus, setPlayerStatus] = useState(false);
+  const [time, setTime] = useState({
+    currentTime: {
+      minute: 0,
+      second: 0,
+    },
+    totalTime: {
+      minute: 0,
+      second: 0,
+    },
+  });
 
+  const play = () => {
+    audioRef.current.play();
+    setPlayerStatus(true);
+  };
+  const pause = () => {
+    audioRef.current.pause();
+    setPlayerStatus(false);
+  };
+
+  const playWithId = async (id) => {
+    await setTrack(songsData[id]);
+    await audioRef.current.play();
+    setPlayerStatus(true);
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      audioRef.current.ontimeupdate = () => {
+        seekBar.current.style.width =
+          Math.floor(
+            (audioRef.current.currentTime / audioRef.current.duration) * 100
+          ) + "%";
+        setTime({
+          currentTime: {
+            minute: Math.floor(audioRef.current.currentTime / 60),
+            second: Math.floor(audioRef.current.currentTime % 60),
+          },
+          totalTime: {
+            minute: Math.floor(audioRef.current.duration / 60),
+            second: Math.floor(audioRef.current.duration % 60),
+          },
+        });
+      };
+    }, 1000);
+  }, [audioRef]);
   const ContextValue = {
     audioRef,
+    seekBar,
+    seekBg,
+    track,
+    setTrack,
+    playerStatus,
+    setPlayerStatus,
+    time,
+    setTime,
+    play,
+    pause,
+    playWithId,
   };
 
   return (
